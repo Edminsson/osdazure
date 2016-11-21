a86eaa4116eb028f4d2385038b4ae93dfd1a8813
Edminsson <axel.aguilar@gmail.com>
2016-11-21 16:16:28 +0100

    global functions out of ready

--- public/javascripts/zoom-click.js
+++ public/javascripts/zoom-click.js
@@ -1,19 +1,19 @@
-$(function() {
 function showClickToZoomStatus() {
-        if(openSeadragonViewer.gestureSettingsMouse.clickToZoom) {
-            $("#clickToZoomLbl").text("clickToZoom är aktiverat");
-        } else {
-            $("#clickToZoomLbl").text("clickToZoom är inaktiverat");            
-        }
+    if(openSeadragonViewer.gestureSettingsMouse.clickToZoom) {
+        $("#clickToZoomLbl").text("clickToZoom är aktiverat");
+    } else {
+        $("#clickToZoomLbl").text("clickToZoom är inaktiverat");            
     }
-    function showDblClickToZoomStatus() {
-        if(openSeadragonViewer.gestureSettingsMouse.dblClickToZoom) {
-            $("#dblClickToZoomLbl").text("dblClickToZoom är aktiverat");
-        } else {
-            $("#dblClickToZoomLbl").text("dblClickToZoom är inaktiverat");            
-        }
+}
+function showDblClickToZoomStatus() {
+    if(openSeadragonViewer.gestureSettingsMouse.dblClickToZoom) {
+        $("#dblClickToZoomLbl").text("dblClickToZoom är aktiverat");
+    } else {
+        $("#dblClickToZoomLbl").text("dblClickToZoom är inaktiverat");            
     }
+}
 
+$(function() {
     $('#clickToZoomBtn').on('click', function() {
         openSeadragonViewer.gestureSettingsMouse.clickToZoom = !openSeadragonViewer.gestureSettingsMouse.clickToZoom;
         showClickToZoomStatus();
