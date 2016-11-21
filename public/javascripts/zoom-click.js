a7892ef9520ff5f3edfc4b85b7c1877cce98cd1a
Edminsson <axel.aguilar@gmail.com>
2016-11-21 16:14:45 +0100

    moving functions to ready

--- public/javascripts/zoom-click.js
+++ public/javascripts/zoom-click.js
@@ -1,24 +1,28 @@
+$(function() {
 function showClickToZoomStatus() {
-    if(openSeadragonViewer.gestureSettingsMouse.clickToZoom) {
-        $("#clickToZoomLbl").text("clickToZoom är aktiverat");
-    } else {
-        $("#clickToZoomLbl").text("clickToZoom är inaktiverat");            
+        if(openSeadragonViewer.gestureSettingsMouse.clickToZoom) {
+            $("#clickToZoomLbl").text("clickToZoom är aktiverat");
+        } else {
+            $("#clickToZoomLbl").text("clickToZoom är inaktiverat");            
+        }
     }
-}
-function showDblClickToZoomStatus() {
-    if(openSeadragonViewer.gestureSettingsMouse.dblClickToZoom) {
-        $("#dblClickToZoomLbl").text("dblClickToZoom är aktiverat");
-    } else {
-        $("#dblClickToZoomLbl").text("dblClickToZoom är inaktiverat");            
+    function showDblClickToZoomStatus() {
+        if(openSeadragonViewer.gestureSettingsMouse.dblClickToZoom) {
+            $("#dblClickToZoomLbl").text("dblClickToZoom är aktiverat");
+        } else {
+            $("#dblClickToZoomLbl").text("dblClickToZoom är inaktiverat");            
+        }
     }
-}
 
-$('#clickToZoomBtn').on('click', function() {
-    openSeadragonViewer.gestureSettingsMouse.clickToZoom = !openSeadragonViewer.gestureSettingsMouse.clickToZoom;
-    showClickToZoomStatus();
-});  
+    $('#clickToZoomBtn').on('click', function() {
+        openSeadragonViewer.gestureSettingsMouse.clickToZoom = !openSeadragonViewer.gestureSettingsMouse.clickToZoom;
+        showClickToZoomStatus();
+    });  
 
-$('#dblClickToZoomBtn').on('click', function() {
-    openSeadragonViewer.gestureSettingsMouse.dblClickToZoom = !openSeadragonViewer.gestureSettingsMouse.dblClickToZoom;
-    showDblClickToZoomStatus();
-});  
+    $('#dblClickToZoomBtn').on('click', function() {
+        openSeadragonViewer.gestureSettingsMouse.dblClickToZoom = !openSeadragonViewer.gestureSettingsMouse.dblClickToZoom;
+        showDblClickToZoomStatus();
+    });  
+});
+
+    
