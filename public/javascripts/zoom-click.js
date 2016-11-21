616d9d57b435703839323c0863f770242f8cd00e
Edminsson <axel.aguilar@gmail.com>
2016-11-21 16:03:33 +0100

    adding clickToZoom operations

---
+++ public/javascripts/zoom-click.js
@@ -0,0 +1,21 @@
+(function(){
+
+    function showClickToZoomStatus() {
+        if(openSeadragonViewer.gestureSettingsMouse.clickToZoom) {
+            $("#clickToZoomLbl").text("clickToZoom är aktiverat");
+        } else {
+            $("#clickToZoomLbl").text("clickToZoom är inaktiverat");            
+        }
+    }
+    function showDblClickToZoomStatus() {
+        if(openSeadragonViewer.gestureSettingsMouse.dblClickToZoom) {
+            $("#dblClickToZoomLbl").text("dblClickToZoom är aktiverat");
+        } else {
+            $("#dblClickToZoomLbl").text("dblClickToZoom är inaktiverat");            
+        }
+    }
+
+    showClickToZoomStatus();
+    showClickToZoomStatus();
+
+})();
